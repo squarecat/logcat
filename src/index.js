e5885022729721d8e7a607b376cd85908e6bfd8d
James Ivings <james@squarecat.io>
2022-02-08 14:22:22 +0700

    added json log to file for datadog

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
-const { Client } = require('@elastic/elasticsearch');
-const { NodeLogger, ImapLogger } = require('./loggers');
+const { NodeLogger } = require("./loggers");
 
-let client;
-
-let loggers = {};
-
-function init({ url, auth = {}, index, name }) {
-  if (url) {
-    client = new Client({
-      node: url,
-      auth: {
-        username: auth.username,
-        password: auth.password
-      },
-      sniffOnStart: false
-    });
-    client.__index = index;
-    client.__name = name;
-  }
-  module.exports.nodeLogger = NodeLogger({ index, client });
-  return loggers;
+function init({ name }) {
+  module.exports.nodeLogger = NodeLogger({ name });
 }
 
 module.exports = {
-  init
+  init,
 };
--- src/loggers/index.js
+++ src/loggers/index.js
@@ -1,25 +1,25 @@
-const winston = require('winston');
-const { ElasticsearchTransport } = require('@squarecat/winston-elasticsearch');
-const debug = require('debug');
-const nodeLogsMapping = require('./node-mapping');
+const winston = require("winston");
+const debug = require("debug");
 
 winston.exitOnError = false;
-const formatLog = winston.format.printf(({ timestamp, level, message, stack }) => {
-  let msg = message;
-  if (message.constructor === Object) {
-    msg = JSON.stringify(message, null, 4);
+const formatLog = winston.format.printf(
+  ({ timestamp, level, message, stack }) => {
+    let msg = message;
+    if (message.constructor === Object) {
+      msg = JSON.stringify(message, null, 4);
+    }
+    if (stack) {
+      return `${timestamp} ${level} ${msg}\n${stack}`;
+    }
+    return `${timestamp} ${level} ${msg}`;
   }
-  if (stack) {
-    return `${timestamp} ${level} ${msg}\n${stack}`;
-  }
-  return `${timestamp} ${level} ${msg}`;
-});
+);
 
 const filterDebug = winston.format((log) => {
   const { level, message } = log;
 
-  if (level !== 'debug' || debug.enabled('all')) return log;
-  if (!message || typeof message !== 'string') return log;
+  if (level !== "debug" || debug.enabled("all")) return log;
+  if (!message || typeof message !== "string") return log;
 
   const matchDebug = message.match(/\[(.+)\]/);
   if (!matchDebug || debug.enabled(matchDebug[1])) {
@@ -28,50 +28,10 @@ const filterDebug = winston.format((log) => {
   return false;
 });
 
-function nodeTransformer({ timestamp, level, message, meta: originalMeta }) {
-  try {
-    const name = this.client.__name;
-    let meta = originalMeta || {};
-    let m = message;
-    let service = 'unknown';
-    if (typeof message !== 'string') {
-      if (message instanceof Error) {
-        m = message.message;
-        meta = {
-          ...meta,
-          stack: message.stack
-        };
-      } else if (message.toString() !== '[Object object]') {
-        m = message.toString;
-      } else {
-        return;
-      }
-    } else {
-      const matchService = m.match(/^\[(.+)\]: (.*)/);
-      if (matchService) {
-        service = matchService[1];
-        m = matchService[2];
-      }
-    }
-
-    return {
-      '@timestamp': Date.now(),
-      ingest_time: Date.now(),
-      severity: level,
-      message: m,
-      meta,
-      service,
-      system_name: name
-    };
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 const winstonOptions = {
   format: winston.format.combine(
     winston.format.timestamp({
-      format: 'DD-MM-YYYY HH:mm:ss'
+      format: "DD-MM-YYYY HH:mm:ss",
     }),
     winston.format.errors({ stack: true }),
     winston.format.json()
@@ -84,48 +44,38 @@ const winstonOptions = {
         winston.format.colorize(),
         winston.format.simple(),
         formatLog
-      )
-    })
-  ]
+      ),
+    }),
+  ],
 };
 
-const NodeLogger = function ({ index, client, name }) {
+const NodeLogger = function ({ name }) {
   const logger = winston.createLogger({
-    level: 'debug',
+    level: "debug",
     levels: winston.config.syslog.levels,
-    ...winstonOptions
+    ...winstonOptions,
   });
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     logger.add(
       new winston.transports.File({
-        filename: 'error.log',
-        level: 'error',
-        format: winston.format.combine(filterDebug(), formatLog)
+        filename: "error.log",
+        level: "error",
+        format: winston.format.combine(filterDebug(), formatLog),
       })
     );
     logger.add(
       new winston.transports.File({
-        filename: 'combined.log',
-        format: winston.format.combine(filterDebug(), formatLog)
+        filename: "combined.log",
+        format: winston.format.combine(filterDebug(), formatLog),
       })
     );
-    if (client) {
-      const est = new ElasticsearchTransport({
-        level: 'debug',
-        index: client.__index,
-        transformer: nodeTransformer,
-        flushInterval: 5000,
-        bufferLimit: 200,
-        client,
-        handleExceptions: true,
-        handleRejections: true,
-        exitOnError: false
-      });
-      est.on('error', (err) => {
-        console.error(err.message);
-      });
-      logger.add(est);
-    }
+    logger.add(
+      new winston.transports.File({
+        filename: `/var/www/logs/${name}.log`,
+        filename: "combined.log",
+        format: format.json(),
+      })
+    );
   }
 
   if (!logger.warning) {
